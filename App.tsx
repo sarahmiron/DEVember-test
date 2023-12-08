952620f1af9dcd0be0d20eafd28d010cf67b0c18
sarahmiron <sarah.miron11@gmail.com>
2023-12-08 14:50:05 +0200

    list of numbers

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+const days = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <Text>DEVember</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={days}
+        renderItem={({ item }) => (<View style={styles.box}>
+          <Text style={styles.text}>{item}
+          </Text>
+        </View>)}
+      />
+
     </View>
   );
 }
@@ -17,5 +24,26 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
   },
+
+  box:
+  {
+    backgroundColor: '#F9EDE3',
+    width: 300,
+    height: 300,
+
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: '#9b4521',
+    borderRadius: 20,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: '#9b4521',
+    fontSize: 50,
+  },
+
+
 });
