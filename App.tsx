031702076d128daef6fd953dbd3dc9d663187647
baleasarah <sarah.balea@ulbsibiu.ro>
2024-02-26 11:31:37 +0200

    Advent calender list

--- App.tsx
+++ App.tsx
@@ -1,19 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-
-const days = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+import DayListitem from './src/components/core/DayListitem';
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
       <FlatList
         data={days}
-        renderItem={({ item }) => (<View style={styles.box}>
-          <Text style={styles.text}>{item}
-          </Text>
-        </View>)}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }) => <DayListitem day={item} />}
       />
-
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -22,28 +22,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    gap: 10,
   },
-
-  box:
+  content:
   {
-    backgroundColor: '#F9EDE3',
-    width: 300,
-    height: 300,
-
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: '#9b4521',
-    borderRadius: 20,
-
-    justifyContent: 'center',
-    alignItems: 'center',
+    gap: 10,
+    padding: 10,
   },
-  text: {
-    color: '#9b4521',
-    fontSize: 50,
-  },
+  column: {
+    gap: 10,
 
+  }
 
 });
